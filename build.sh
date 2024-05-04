43cfb47fb4e6c91663c8ef7921c01a08907cf347
Akeal <nicksattler10@gmail.com>
2024-05-04 13:03:46 -0400

    modified:   NugetSource/Godot.NET.Sdk.4.3.100-dev.nupkg 	modified:   NugetSource/Godot.SourceGenerators.4.3.100-dev.nupkg 	modified:   NugetSource/GodotSharp.4.3.100-dev.nupkg 	modified:   NugetSource/GodotSharp.4.3.100-dev.snupkg 	modified:   NugetSource/GodotSharpEditor.4.3.100-dev.nupkg 	modified:   NugetSource/GodotSharpEditor.4.3.100-dev.snupkg 	modified:   build.sh 	modified:   scene/main/window.cpp

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 5241b0105b (	modified:   NugetSource/Godot.NET.Sdk.4.3.100-dev.nupkg)
 
 clean="false"
 
@@ -11,13 +14,17 @@ while (( $# >= 1 )); do
     shift
 done
 
+<<<<<<< HEAD
 =======
 >>>>>>> d76c8750af (post_process and build)
+=======
+>>>>>>> 5241b0105b (	modified:   NugetSource/Godot.NET.Sdk.4.3.100-dev.nupkg)
 scriptPath=$(realpath .)
 nugetPath="/usr/local/bin/nuget.exe"
 nugetConfigPath="/home/nick/.nuget/NuGet/NuGet.Config"
 nugetSource="${scriptPath}/NugetSource"
 nugetSources=$(eval "mono ${nugetPath} sources list -ConfigFile ${nugetConfigPath}")
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 #echo "$scriptPath"
@@ -41,11 +48,32 @@ echo "${scriptPath}/bin/godot.linuxbsd.editor.x86_64.mono --generate-mono-glue $
 scons platform=linuxbsd module_mono_enabled=yes
 #echo "${scriptPath}/bin/godot.linuxbsd.editor.x86_64.mono --generate-mono-glue ${scriptPath}/modules/mono/glue" | bash
 >>>>>>> d76c8750af (post_process and build)
+=======
+
+#echo "$scriptPath"
+#echo "$nugetSource"
+#echo "$nugetSources"
+
+build="scons platform=linuxbsd module_mono_enabled=yes"
+
+if "$clean" == "true"; then
+  echo "Cleaning ..."
+  build="$build -c"
+fi
+eval $"("${build}")"
+
+#if "$clean" == "false"; then
+echo "${scriptPath}/bin/godot.linuxbsd.editor.x86_64.mono --generate-mono-glue ${scriptPath}/modules/mono/glue" | bash
+>>>>>>> 5241b0105b (	modified:   NugetSource/Godot.NET.Sdk.4.3.100-dev.nupkg)
 if ! echo "${nugetSources}" | grep "${nugetSource}"; then
   $(eval "dotnet nuget add source ${nugetSource} --name GodotCppNugetSource")
 fi
 ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local ./NugetSource
 <<<<<<< HEAD
+<<<<<<< HEAD
 #fi
 =======
 >>>>>>> d76c8750af (post_process and build)
+=======
+#fi
+>>>>>>> 5241b0105b (	modified:   NugetSource/Godot.NET.Sdk.4.3.100-dev.nupkg)
